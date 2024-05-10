0f8d9f676e1a492b3fbd1e854a671fd88c0a9861
[Dalin Fangloy] <[d.fangloy@gmail.com]>
2024-05-10 21:07:00 +0200

    Solved lab

--- src/add.js
+++ src/add.js
@@ -1,7 +1,20 @@
-function add (numOne, numTwo) {
+/*function add (numOne, numTwo) {
     if (numOne == undefined || numTwo == undefined) {
         return undefined;
+    } else if (typeof numOne !== "number" || typeof numTwo !== "number") {
+        return undefined;
     }
-
     return numOne + numTwo;
 }
+*/
+
+//Refactored to:
+function add (numOne, numTwo) {
+    if (typeof numOne !== "number" || typeof numTwo !== "number") {
+        return undefined;
+    }
+    return numOne + numTwo;
+}
+
+
+
--- tests/divide.spec.js
+++ tests/divide.spec.js
@@ -1,14 +1,41 @@
-// UNCOMMENT THE CODE BELOW TO START
-
-/*
-
 describe("Iteration 2", () => {
     describe("Function - divide", () => {
         
         // Each `it` block represents 1 test. You can use the following as a template:
-        it("", () => {});
+        it("should be defined.", () => {
+            expect(divide).toBeDefined(2);
+        });
+
+        it("should take two numbers as arguments.", () => {
+            //expect(add.length).toBe(2)
+            //expect(add(1,"a")).toEqual(undefined);
+            expect(divide(1,2)).toBe(1,2);
+            expect(divide(1,"a")).toEqual(undefined);
+
+        });
+
+        it("should return the division of the two numbers.", () => {
+            expect(divide(10,5).toEqual(2))
+            expect(divide(10,0).toEqual(undefined))
+            expect(divide(10,100).toEqual(0.1))
+        });
+            
+
+        it("should return undefined if any of the arguments is not provided.", () => {});
+            expect(divide(10)).toEqual(undefined)
+            expect(divide(5,"")).toEqual(undefined)
+            expect(divide("",5)).toEqual(undefined)
 
     })    
 })
+
+
+
+
+/* 
+The function should be defined.
+The function should take two numbers as arguments.
+The function should return the division of the two numbers.
+The function should return undefined if any of the arguments is not provided
 
 */
